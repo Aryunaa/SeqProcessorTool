53eb42431b119810646e09e3ecc85d19f443ccd7
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2023-10-19 02:58:16 +0300

    change pep8

--- bioinf_tool.py
+++ bioinf_tool.py
@@ -1,14 +1,14 @@
-
 import modules.dna_rna_tools as drt
 import modules.protein_analyzer_tool as prt
 import modules.fastq_tools as ft
 
-#from modules.dna_rna_tools import run_dna_rna_tools as dna_rna
-#from modules.protein_analyzer_tool import run_protein_analyzer_tool as prot
-#from modules.fastq_tools import run_fastq_tools as fastq
+
+# from modules.dna_rna_tools import run_dna_rna_tools as dna_rna
+# from modules.protein_analyzer_tool import run_protein_analyzer_tool as prot
+# from modules.fastq_tools import run_fastq_tools as fastq
 
 
-def run_dna_rna_tools(*seqs,tool='transcribe'):
+def run_dna_rna_tools(*args, tool='transcribe'):
     """
     Works with dna or rna sequences, has 4 options to work:
         - get transcribed sequence (`transcribe` operation);
@@ -17,43 +17,33 @@ def run_dna_rna_tools(*seqs,tool='transcribe'):
         - get reversed complementary sequence (`reverse_complement` operation);
 
     Args:
-        *seqs (str): various number of nucleotide sequences
+        *args (str): various number of nucleotide sequences
         tool (str): one desired option to work.
 
     Returns tuple containing two list:
         result (list): list with operation results for each valid sequence;
     """
-
+    seqs = list(args)
     is_dna_or_rna_list = drt.is_dna_or_rna(seqs)
-    if(True in is_dna_or_rna_list):
+    if any(is_dna_or_rna_list):
 
-        if (tool == 'transcribe'):
-            answer = drt.transcribe(seqs,is_dna_or_rna_list = is_dna_or_rna_list)
-        elif (tool == 'reverse'):
-            answer = drt.reverse(seqs,is_dna_or_rna_list = is_dna_or_rna_list)
-        elif (tool == 'complement'):
-            answer = drt.complement(seqs,is_dna_or_rna_list = is_dna_or_rna_list)
-        elif (tool == 'reverse_complement'):
-            answer = drt.reverse_complement(seqs, is_dna_or_rna_list = is_dna_or_rna_list)
-        print(answer)
+        if tool == 'transcribe':
+            answer = drt.transcribe(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        elif tool == 'reverse':
+            answer = drt.reverse(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        elif tool == 'complement':
+            answer = drt.complement(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        elif tool == 'reverse_complement':
+            answer = drt.reverse_complement(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        else:
+            answer = 'Please enter the proper option'
+        return answer
+
     else:
-        print('Neither DNA nor RNA was found,please provide DNA or RNA')
-
-#prot()
+        return 'Neither DNA nor RNA was found,please provide DNA or RNA'
 
 
-
-#fastq()
-
+# prot()
 
 
-def my_test():
-    #run_dna_rna_tools('aTgcatgCaUgcatgc','atgc','aTgc',tool='transcribe')
-    #run_dna_rna_tools('aTgcatgCaUgcatgc',tool='transcribe')
-    #run_dna_rna_tools('aTgcatgCaUgcatgc','atgc','aTgc',tool='reverse')
-    #run_dna_rna_tools('aTgcatgCaUgcatgc',tool='reverse')
-
-    run_dna_rna_tools('aTgcatgCaUgcatgc','atgc','aTgc',tool='complement')
-    run_dna_rna_tools('aTgcatgCaUgcatgc',tool='complement')
-    run_dna_rna_tools('aTgcatgCaUgcatgc','atgc','aTgc',tool='reverse_complement')
-    run_dna_rna_tools('aTgcatgCaUgcatgc',tool='reverse_complement')
+# fastq()
--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -1,52 +1,62 @@
+def is_dna_or_rna(*args, alphabet_dna={'A', 'T', 'G', 'C', 'a', 't', 'g', 'c'},
+                  alphabet_rna={'A', 'U', 'G', 'C', 'a', 'u', 'g', 'c'}) -> list:
+    """
+    works
 
-def is_dna_or_rna(*seqs,alphabet_dna={'A', 'T', 'G', 'C','a','t','g','c'}, alphabet_rna = {'A', 'U', 'G', 'C','a','u','g','c'}):
-    seqs = list(*seqs)
+    :param args:
+
+    :return:
+    """
+    seqs = list(*args)
     is_dna_or_rna_list = []
     for seq in seqs:
         unique_chars = set(seq)
-        if(unique_chars<=alphabet_dna):
-            is_dna_or_rna_list.append(1) # 1 means dna
-        elif(unique_chars<=alphabet_rna):
-            is_dna_or_rna_list.append(2) # 2 means rna
+        if unique_chars <= alphabet_dna:
+            is_dna_or_rna_list.append('dna')  # 1 means dna
+        elif unique_chars <= alphabet_rna:
+            is_dna_or_rna_list.append('rna')  # 2 means rna
         else:
-            is_dna_or_rna_list.append(0)
-    return (is_dna_or_rna_list)
+            is_dna_or_rna_list.append(False)
+    return is_dna_or_rna_list
 
-def transcribe(*seqs,is_dna_or_rna_list):
-    '''
+
+def transcribe(*args, is_dna_or_rna_list) -> list:
+    """
     works
 
-    :param seqs:
+    :param args:
     :return:
-    '''
-    seqs = list(*seqs)
+    """
+    seqs = list(*args)
     res_seq = []
+
     for i in range(len(seqs)):
-        if(is_dna_or_rna_list[i]==True):
+        if is_dna_or_rna_list[i] is True:
             seq = seqs[i]
-            if ('T' in seq):
-                temp_seq= seq.replace('T', 'U')
-            elif ('t' in seq):
-                temp_seq = seq.replace('t','u')
+            if 'T' in seq:
+                temp_seq = seq.replace('T', 'U')
+            elif 't' in seq:
+                temp_seq = seq.replace('t', 'u')
         else:
-            temp_seq='Please provide DNA or RNA'
+            temp_seq = 'Please provide DNA or RNA'
         res_seq.append(temp_seq)
     if len(res_seq) == 1:
         return res_seq[0]
     else:
         return res_seq
 
-def reverse(*seqs,is_dna_or_rna_list):
-    '''
+
+def reverse(*args, is_dna_or_rna_list) -> list:
+    """"
 
     works
     :param seqs:
     :return:
-    '''
-    seqs = list(*seqs)
+    """
+    seqs = list(*args)
     rev_seq = []
     for i in range(len(seqs)):
-        if (is_dna_or_rna_list[i] == True):
+        if is_dna_or_rna_list[i] is True:
             seq = seqs[i]
             rev_seq.append(seq[::-1])
         else:
@@ -54,143 +64,75 @@ def reverse(*seqs,is_dna_or_rna_list):
     if len(rev_seq) == 1:
         return rev_seq[0]
     else:
-        return rev_seq
+        return [rev_seq]
 
-def complement(*seqs,is_dna_or_rna_list):
-    seqs = list(*seqs)
+
+def complement(*args, is_dna_or_rna_list,
+               dna_complement_nucl={"A": "T", "G": "C", "C": "G", "T": "A", "a": "t", "g": "c", "c": "g", "t": "a"},
+               rna_complement_nucl={"A": "U", "G": "C", "C": "G", "U": "A", "a": "u", "g": "c", "c": "g","u": "a"}) -> list:
+    seqs = list(*args)
     comp_seqs = []
     for i in range(len(seqs)):
-        if (is_dna_or_rna_list[i] == 1): # if seq is dna
+        if is_dna_or_rna_list[i] == 'dna':  # if seq is dna
             seq = seqs[i]
             comp_seq = ''
             print(seq)
-            for j in seq:
-                if(j == 'A'):
-                    comp_seq += 'T'
-                elif(j == 'T'):
-                    comp_seq += 'A'
-                elif(j == 'G'):
-                    comp_seq += 'C'
-                elif(j == 'C'):
-                    comp_seq += 'G'
+            for nucl in seq:
+                comp_seq += dna_complement_nucl[nucl]
 
-                if(j == 'a'):
-                    comp_seq += 't'
-                elif(j == 't'):
-                    comp_seq += 'a'
-                elif(j == 'g'):
-                    comp_seq += 'c'
-                elif(j == 'c'):
-                    comp_seq += 'g'
-
-        elif (is_dna_or_rna_list[i] == 2): # if seq is rna
+        elif is_dna_or_rna_list[i] == 'rna':  # if seq is rna
             print('rna')
             seq = seqs[i]
             comp_seq = ''
-            for j in seq:
-                if (j == 'A'):
-                    comp_seq += 'U'
-                elif (j == 'U'):
-                    comp_seq += 'A'
-                elif (j == 'G'):
-                    comp_seq += 'C'
-                elif (j == 'C'):
-                    comp_seq += 'G'
-
-                if (j == 'a'):
-                    comp_seq += 'u'
-                elif (j == 'u'):
-                    comp_seq += 'a'
-                elif (j == 'g'):
-                    comp_seq += 'c'
-                elif (j == 'c'):
-                    comp_seq += 'g'
-
-        else:
-            comp_seq='Please provide DNA or RNA'
-
-        comp_seqs.append(comp_seq)
-    if len(comp_seqs) == 1:
-        return comp_seqs[0]
-    else:
-        return comp_seqs
-
-def reverse_complement(*seqs,is_dna_or_rna_list):
-    #################################################
-    seqs = list(*seqs)
-    comp_seqs = []
-    for i in range(len(seqs)):
-        if (is_dna_or_rna_list[i] == 1):  # if seq is dna
-            seq = seqs[i]
-            comp_seq = ''
-            print(seq)
-            for j in seq:
-                if (j == 'A'):
-                    comp_seq += 'T'
-                elif (j == 'T'):
-                    comp_seq += 'A'
-                elif (j == 'G'):
-                    comp_seq += 'C'
-                elif (j == 'C'):
-                    comp_seq += 'G'
-
-                if (j == 'a'):
-                    comp_seq += 't'
-                elif (j == 't'):
-                    comp_seq += 'a'
-                elif (j == 'g'):
-                    comp_seq += 'c'
-                elif (j == 'c'):
-                    comp_seq += 'g'
-
-        elif (is_dna_or_rna_list[i] == 2):  # if seq is rna
-            print('rna')
-            seq = seqs[i]
-            comp_seq = ''
-            for j in seq:
-                if (j == 'A'):
-                    comp_seq += 'U'
-                elif (j == 'U'):
-                    comp_seq += 'A'
-                elif (j == 'G'):
-                    comp_seq += 'C'
-                elif (j == 'C'):
-                    comp_seq += 'G'
-
-                if (j == 'a'):
-                    comp_seq += 'u'
-                elif (j == 'u'):
-                    comp_seq += 'a'
-                elif (j == 'g'):
-                    comp_seq += 'c'
-                elif (j == 'c'):
-                    comp_seq += 'g'
+            for nucl in seq:
+                comp_seq += rna_complement_nucl[nucl]
 
         else:
             comp_seq = 'Please provide DNA or RNA'
 
         comp_seqs.append(comp_seq)
-    #######################################S
 
-    rev_comp_seqs = []
-    for i in range(len(comp_seqs)):
-        if (is_dna_or_rna_list[i] == True):
-            seq = comp_seqs[i]
-            rev_comp_seqs.append(seq[::-1])
+    return comp_seqs
+
+
+def reverse_complement(*args, is_dna_or_rna_list) -> list:
+    seqs = list(args)
+    comp_seqs = complement(*seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+    rev_comp_seqs = reverse(*comp_seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+
+    return rev_comp_seqs
+
+
+def run_dna_rna_tools(*args, tool='transcribe') -> list:
+    """
+    Works with dna or rna sequences, has 4 options to work:
+        - get transcribed sequence (`transcribe` operation);
+        - get reversed sequence (`reverse` operation);
+        - get complementary sequence (`complement` operation);
+        - get reversed complementary sequence (`reverse_complement` operation);
+
+    Args:
+        *args (str): various number of nucleotide sequences
+        tool (str): one desired option to work.
+
+    Returns tuple containing two list:
+        result (list): list with operation results for each valid sequence;
+    """
+    seqs = list(args)
+    is_dna_or_rna_list = is_dna_or_rna(seqs)
+    if any(is_dna_or_rna_list):
+
+        if tool == 'transcribe':
+            answer = transcribe(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        elif tool == 'reverse':
+            answer = reverse(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        elif tool == 'complement':
+            answer = complement(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
+        elif tool == 'reverse_complement':
+            answer = reverse_complement(seqs, is_dna_or_rna_list=is_dna_or_rna_list)
         else:
-            rev_comp_seqs.append('Please provide DNA or RNA')
+            answer = 'Please enter the proper option'
+        return answer
 
-    if len(rev_comp_seqs) == 1:
-        return rev_comp_seqs[0]
     else:
-        return rev_comp_seqs
-
-
-
-
-
-
-
-
-#my_test()
-#коммит для автотеста
+        return ['Neither DNA nor RNA was found,please provide DNA or RNA']
